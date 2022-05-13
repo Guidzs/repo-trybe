2b1d2253d9d01f1fe97ea955aff6721c838d3728
Guidzs <guidz.2004@gmail.com>
2022-05-13 15:28:32 -0300

    adiciona pasta para o dia 2 java

--- trybe-exercicios/JavaScript/D1-recursos/exercicios if else/scripts.js
+++ trybe-exercicios/JavaScript/D1-recursos/exercicios if else/scripts.js
@@ -115,9 +115,9 @@ const c = 5
 
 //// parte 8
 
-if ((a % 2) === 0 || (b % 2) === 0 || (c % 2) === 0) {
-    console.log(true)
-} 
-else {
-    console.log(false)
-}
+// if ((a % 2) === 0 || (b % 2) === 0 || (c % 2) === 0) {
+//     console.log(true)
+// } 
+// else {
+//     console.log(false)
+// }
