fa4250a9589b2aa2a4c4dbc1a44d79ce5ed6c47a
Guidzs <guidz.2004@gmail.com>
2022-05-16 12:56:51 -0300

    exercicios 5 e 6

--- trybe-exercicios/JavaScript/D2-arreys e for/exercicios.js
+++ trybe-exercicios/JavaScript/D2-arreys e for/exercicios.js
@@ -1,9 +1,12 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let numbersSoma = 0;
+let largestNumber = 0;
+let imparNumbers = 0;
+let smallestNumber = 0;
 
 // console.log(numbers)
 
-// for (let index = 0; index < numbers.length; index += 1) {
+// for (let index2 = 0; index < numbers.length; index += 1) {
 //     numbersSoma = numbersSoma + numbers[index]
 // }
 // console.log(numbersSoma);
@@ -15,3 +18,30 @@ let numbersSoma = 0;
 // else {
 //     console.log("valor menor ou igual a 20")
 // }
+
+// for (let index = 0; index < numbers.length; index += 1) {
+//       if (numbers[index] > largestNumber) {
+//         largestNumber = numbers[index]
+//       } 
+// }
+// console.log(largestNumber)
+
+// for (let index = 0; index < numbers.length; index += 1) {
+//     if ((numbers[index] % 2) === 1) {
+//         imparNumbers += 1
+//     }
+// }
+// if (imparNumbers > 1) {
+//     console.log(imparNumbers)
+// } else {
+//     console.log('Nenhum valor ímpar encontrado')
+// }
+
+for (let index = 0; index < numbers.length; index += 1) {
+    for (let index2 = 0; index2 < numbers.length; index2 += 1) {
+        if (numbers[index] < numbers[index2]) {
+            smallestNumber = numbers[index]
+         }          
+      }
+}
+console.log(smallestNumber)
