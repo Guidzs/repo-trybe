ad0c88ee24da4699133fb0a0b47669fcc220a49f
Guidzs <guidz.2004@gmail.com>
2022-05-18 11:44:42 -0300

    exercicios 1 a 5

---
+++ trybe-exercicios/JavaScript/D4-objetos-e-função/exercicios.js
@@ -0,0 +1,36 @@
+// let infoMargarida = {
+//   personagem: 'Margarida',
+//   origem: 'Pato Donald',
+//   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+//   recorrente: 'Sim'
+// };
+
+//   console.log('Bem vinda,', info['personagem'])
+
+//   console.log(info)
+
+//   for (let key in info) {
+//     console.log(key)
+//   }
+
+//   for (let key in info) {
+//     console.log(info[key])
+//   }
+
+// let infoTioPatinhas = {
+//   personagem: 'Tio Patinhas',
+//   origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+//   nota: 'O último MacPatinhas',
+//   recorrente: 'Sim'
+// };
+
+
+// for (let key in infoMargarida,infoTioPatinhas) {
+//   if (infoMargarida[key] === 'Sim' && infoTioPatinhas[key] === 'Sim') {
+//     console.log('Ambos recorrentes')
+//   }
+//   else {
+//     console.log(infoMargarida[key] + ' e ' + infoTioPatinhas[key])
+//   }
+// }
+
