93e175ff8f10f55e4fe083d09f6e80db7af91174
Guidzs <guidz.2004@gmail.com>
2022-05-23 15:27:02 -0300

    cria ambiente para atividades 5.3

---
+++ trybe-exercicios/DOM/D2-busca-elementos/atividades/script.js
@@ -0,0 +1,10 @@
+let ondeEuEstou = document.querySelector('#elementoOndeVoceEsta');
+ondeEuEstou.style.color = 'red';
+
+let filhoDoFilho = document.getElementById('primeiroFilhoDoFilho');
+filhoDoFilho.innerText = 'Olá, eu sou o filho';
+
+let pai = document.getElementById('pai').firstChild;
+
+let atenção = ondeEuEstou.parentNode.childNodes[4]
+
