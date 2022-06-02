22f25790ab9bad35310833ef35f76d2fd618525d
Guidzs <guidz.2004@gmail.com>
2022-06-02 15:27:45 -0300

    finaliza

--- trybe-exercicios/form-flex/D2-framework/exercicios/script.js
+++ trybe-exercicios/form-flex/D2-framework/exercicios/script.js
@@ -1,9 +1,5 @@
 let buttonSend = document.querySelector(".button")
-let bestDate = document.querySelector("#best-date")
 
-bestDate.addEventListener("click", function() {
-    bestDate.style.border = cadetblue
-})
 
 function prevDefault(event) {
     event.preventDefault()
