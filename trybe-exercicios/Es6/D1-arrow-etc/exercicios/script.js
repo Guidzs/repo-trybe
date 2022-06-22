b92524fe8391dbbd067704c075b100f176b905fb
Guidzs <guidz.2004@gmail.com>
2022-06-22 14:13:11 -0300

    ES6

--- trybe-exercicios/Es6/D1-arrow-etc/exercicios/script.js
+++ trybe-exercicios/Es6/D1-arrow-etc/exercicios/script.js
@@ -1,13 +1,33 @@
-const testingScope = (escopo) => {
-    if (escopo === true) {
-      let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
-      ifScope =`${ifScope} ótimo, fui utilizada no escopo !`;
-      console.log(ifScope);
-    } else {
-      let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
-      console.log(elseScope);
-    }
-    
-  }
+const btnCount = document.querySelector('#btn-count');
+const h1Count = document.querySelector('#h1-count');
+let clickCount = 0
 
-  testingScope(true);
+btnCount.addEventListener('click', () => (clickCount += 1) (h1Count.innerHTML = clickCount))
+
+
+
+// //Exercicios
+
+// const testingScope = (escopo) => {
+//   if (escopo === true) {
+//     let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+//     ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
+//     console.log(ifScope);
+//   } else {
+//     let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+//     console.log(elseScope);
+//   }
+// }
+// testingScope(true);
+
+
+// const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+// oddsAndEvens.sort(( a, b) => a - b)
+
+// console.log(`Os números ${oddsAndEvens} se encontram ordenados de forma crescente!`);
+
+
+// const fatorial = (num) => (num > 1) ? num * fatorial(num - 1) : 1
+// console.log(fatorial(5));
+
