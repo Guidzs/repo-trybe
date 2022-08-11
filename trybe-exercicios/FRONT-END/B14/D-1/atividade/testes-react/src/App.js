198db9a33590b249aa0d8f45e3b499d07b54848b
Guidzs <guidz.2004@gmail.com>
2022-08-11 15:18:10 -0300

    atividade de React testes

---
+++ trybe-exercicios/FRONT-END/B14/D-1/atividade/testes-react/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ValidEmail from './components/ValidEmail';
+
+function App() {
+  return (
+    <div className="App">
+      <ValidEmail email="email@email.com"/>
+    </div>
+  );
+}
+
+export default App;
